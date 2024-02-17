c96ece36df85bc262d9bec8ab2e46ee35cf601cc
Velizar Dabov <velizardabov@gmail.com>
2024-02-17 16:41:07 +0200

    profile page

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
+    <section className="bg-gray-200  bg-cover bg-center py-5 md:py-10 ">
+        <div className='flex justify-center items-center'>
+        <h3 className=" h3-bold text-center sm:text-left ">
+      Create Profile page (in progress)
+    </h3>
+        </div>
+   
+  </section>
   )
 }
 
